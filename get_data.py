1fa6e62f14c83b8c32a18772048b82786aa2153c
Clint Olson <coandco@gmail.com>
2019-11-30 21:42:24 -0800

    Add a couple of utility functions for general use

---
+++ get_data.py
@@ -0,0 +1,14 @@
+from aocd import get_data
+from pathlib import Path
+
+# This is a standalone script meant to be run to automatically grab my data
+# for a given year/day and dump it into an automatically-named file.
+
+YEAR_NUM = 2019
+DAY_NUM = 1
+
+data = get_data(year=YEAR_NUM, day=DAY_NUM)
+
+file_location = Path(f'inputs/advent{YEAR_NUM}_day{DAY_NUM:02d}_input.txt')
+with file_location.open(mode='w') as data_file:
+    data_file.write(data)
