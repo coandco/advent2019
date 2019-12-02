b05b3b6d80d4030de21b5bd7f23f6df2e6a329b2
Clint Olson <coandco@gmail.com>
2019-12-01 22:29:02 -0800

    Clean up day 2 and make it extensible for likely future days

--- advent2019_day02.py
+++ advent2019_day02.py
@@ -3,49 +3,83 @@ from utils import read_data
 DATA = [int(x) for x in read_data(__file__).split(",")]
 
 
+class OpCodeBase(object):
+    length = 0
+
+    def __init__(self, tape, index):
+        self.tape = tape
+        self.index = index
+        self.instruction = tape[index:index+self.length]
+
+    def run(self):
+        pass
+
+
+class OpcodeAdd(OpCodeBase):
+    length = 4
+
+    def run(self):
+        pos_one, pos_two, result_pos = self.instruction[1:self.length]
+        self.tape[result_pos] = self.tape[pos_one] + self.tape[pos_two]
+        return self.index + self.length
+
+
+class OpcodeMultiply(OpCodeBase):
+    length = 4
+
+    def run(self):
+        pos_one, pos_two, result_pos = self.instruction[1:self.length]
+        self.tape[result_pos] = self.tape[pos_one] * self.tape[pos_two]
+        return self.index + self.length
+
+
+class OpcodeHalt(OpCodeBase):
+    length = 1
+
+    def run(self):
+        return None  # Halt processing
+
+
+OPCODES = {
+    1: OpcodeAdd,
+    2: OpcodeMultiply,
+    99: OpcodeHalt
+}
+
+
+def process_instruction(tape, index):
+    opcode = tape[index]
+    if opcode in OPCODES:
+        return OPCODES[opcode](tape, index).run()
+    else:
+        raise Exception(f"Unknown opcode {opcode} at position {index} on {tape}")
+
+
 def run_tape(tape):
     curpos = 0
-    while True:
-        if tape[curpos] == 1:
-            pos_one = tape[curpos+1]
-            pos_two = tape[curpos+2]
-            result_pos = tape[curpos+3]
-            tape[result_pos] = tape[pos_one] + tape[pos_two]
-            curpos += 4
-        elif tape[curpos] == 2:
-            pos_one = tape[curpos + 1]
-            pos_two = tape[curpos + 2]
-            result_pos = tape[curpos + 3]
-            tape[result_pos] = tape[pos_one] * tape[pos_two]
-            curpos += 4
-        elif tape[curpos] == 99:
-            break
-        else:
-            print(f'hit invalid opcode, tape looks like {".".join(tape)}')
-            exit(1)
+    while curpos is not None:
+        curpos = process_instruction(tape, curpos)
     return tape
 
 
-def run_tape_two(tape, noun, verb):
+def run_tape_with_params(tape, noun, verb):
     tmptape = tape[:]
     tmptape[1] = noun
     tmptape[2] = verb
     return run_tape(tmptape)
 
 
-part1_data = DATA[:]
-part1_data[1] = 12
-part1_data[2] = 2
-part1_final = run_tape(part1_data)
-print(f"Zero pos is {part1_final[0]}")
+def find_part2_combo(tape, desired_output):
+    for i in range(100):
+        for j in range(100):
+            part2_output = run_tape_with_params(tape, i, j)
+            if part2_output[0] == desired_output:
+                return i, j
+    raise Exception(f"Couldn\'t find noun/verb combination that resulted in {desired_output}")
 
 
-for i in range(100):
-    for j in range(100):
-        part2_output = run_tape_two(DATA, i, j)
-        if part2_output[0] == 19690720:
-            print(f"i is {i}, j is {j}, Answer is {100 * i + j}")
-            exit(0)
+part1_output = run_tape_with_params(DATA, 12, 2)
+print(f"Zero pos is {part1_output[0]}")
 
-
-
+noun, verb = find_part2_combo(DATA, 19690720)
+print(f"noun is {noun}, verb is {verb}, Answer is {100 * noun + verb}")
