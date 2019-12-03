67b36cc6538c2c796b63403967a6f684f2c8ea4d
Clint Olson <coandco@gmail.com>
2019-12-02 22:15:10 -0800

    Add solution for day 3.
    This one was cleaned up prior to committing, rather than
    committed in its base working state and then cleaned up.

---
+++ advent2019_day03.py
@@ -0,0 +1,49 @@
+from utils import read_data
+from collections import defaultdict, namedtuple
+from typing import List, DefaultDict, Dict
+
+
+class Instruction(object):
+    def __init__(self, instruction_string):
+        self.direction = instruction_string[0]
+        self.amount = int(instruction_string[1:])
+
+
+Coord = namedtuple('Coord', ['y', 'x'])
+DIRECTIONS = {'U': Coord(-1, 0), 'R': Coord(0, 1), 'D': Coord(1, 0), 'L': Coord(0, -1)}
+
+
+def go_in_direction(coord: Coord, direction: str) -> Coord:
+    return Coord(x=coord.x+DIRECTIONS[direction].x, y=coord.y+DIRECTIONS[direction].y)
+
+
+def process_instruction(grid: DefaultDict[Coord, Dict], thread_id: int, curpos: Coord, steps: int, instruction: Instruction) -> Coord:
+    for _ in range(instruction.amount):
+        curpos = go_in_direction(curpos, instruction.direction)
+        steps += 1
+        if thread_id not in grid[curpos]:
+            grid[curpos][thread_id] = steps
+    return curpos
+
+
+def process_data(data: List[List[str]]) -> DefaultDict[Coord, Dict]:
+    grid = defaultdict(dict)
+    for thread_id, instruction_list in enumerate(data):
+        current_pos = Coord(y=0, x=0)
+        steps = 0
+        for instruction_str in instruction_list:
+            instruction = Instruction(instruction_str)
+            current_pos = process_instruction(grid, thread_id, current_pos, steps, instruction)
+            steps += instruction.amount
+    return grid
+
+
+if __name__ == '__main__':
+    DATA = [x.split(",") for x in read_data().split("\n")]
+    filled_grid = process_data(DATA)
+    intersections = [key for (key, value) in filled_grid.items() if len(filled_grid[key]) == 2]
+    distances = [abs(coord.y)+abs(coord.x) for coord in intersections]
+    print(f'Smallest Manhattan Distance: {min(distances)}')
+
+    combined_steps = [sum(filled_grid[intersection].values()) for intersection in intersections]
+    print(f'Smallest Combined Steps: {min(combined_steps)}')
