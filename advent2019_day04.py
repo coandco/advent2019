245f1baf845e8bb9f4e9de2b10e8500e1e72da23
Clint Olson <coandco@gmail.com>
2019-12-03 21:48:37 -0800

    Clean up day 4

--- advent2019_day04.py
+++ advent2019_day04.py
@@ -1,49 +1,33 @@
 from utils import read_data
+from collections import Counter
 
 DATA = read_data().split("-")
 
 
 def has_repeated_char(num):
-    numstr = str(num)
-    previous_digit = None
-    for char in numstr:
-        if char == previous_digit:
-            return True
-        else:
-            previous_digit = char
-    return False
+    counts = Counter(str(num))
+    return any((x > 1) for x in counts.values())
 
 
 def meets_part1_criteria(num):
     digits = [int(x) for x in str(num)]
-    if not len(digits) == 6:
-        return False
-    if not has_repeated_char(num):
-        return False
     lowest_digit = 0
     for digit in digits:
         if digit < lowest_digit:
             return False
         else:
             lowest_digit = digit
+
+    # Once we hit this point, we know that the number is "sorted",
+    # which means if there are two of a digit they're guaranteed to be next to each other
+    if not has_repeated_char(num):
+        return False
     return True
 
 
-def meets_part2_criteria(number):
-    numstr = str(number)
-    previous_digit = None
-    current_count = 0
-    for char in numstr:
-        if char == previous_digit:
-            current_count += 1
-        else:
-            if current_count == 2:
-                return True
-            current_count = 1
-            previous_digit = char
-    if current_count == 2:
-        return True
-    return False
+def meets_part2_criteria(num):
+    counts = Counter(str(num))
+    return 2 in counts.values()
 
 
 if __name__ == '__main__':
