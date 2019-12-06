ca1ad8ddac610501bbd373ea15aae01a799d8528
Clint Olson <coandco@gmail.com>
2019-12-05 23:00:23 -0800

    Clean up day 6 a bit

--- advent2019_day06.py
+++ advent2019_day06.py
@@ -2,14 +2,13 @@ from utils import read_data
 from collections import defaultdict
 
 
-def generate_maps(data):
+def generate_reverse_map(data):
     forward_map = defaultdict(list)
     reverse_map = {'COM': None}
     for line in data:
         orbitee, orbiter = line.split(')', 1)
-        forward_map[orbitee].append(orbiter)
         reverse_map[orbiter] = orbitee
-    return forward_map, reverse_map
+    return reverse_map
 
 
 def get_pedigree(reverse_map, object_name):
@@ -30,15 +29,15 @@ def get_path_length(reverse_map, obj_one, obj_two):
     obj_one_pedigree = get_pedigree(reverse_map, obj_one)
     obj_two_pedigree = get_pedigree(reverse_map, obj_two)
     common_ancestor = find_common_ancestor(obj_one_pedigree, obj_two_pedigree)
-    leg_one_length = obj_one_pedigree.index(common_ancestor)
-    leg_two_length = obj_two_pedigree.index(common_ancestor)
     # Subtract one from each leg because we start on a planet and don't need to travel there
-    return leg_one_length-1 + leg_two_length-1
+    leg_one_length = obj_one_pedigree.index(common_ancestor)-1
+    leg_two_length = obj_two_pedigree.index(common_ancestor)-1
+    return leg_one_length + leg_two_length
 
 
 DATA = read_data().split("\n")
 
-forward, reverse = generate_maps(DATA)
+reverse = generate_reverse_map(DATA)
 
 print(f'checksum is {sum(len(get_pedigree(reverse, x))-1 for x in reverse)}')
 
