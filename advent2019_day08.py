e49b94aefa87043f59e9375a81fee17e1899b4b1
Clint Olson <coandco@gmail.com>
2019-12-07 21:45:29 -0800

    Clean up day 8

--- advent2019_day08.py
+++ advent2019_day08.py
@@ -14,38 +14,46 @@ def get_pixel(y, x, layers):
     raise Exception("Transparent all the way down!")
 
 
+def make_layers(incoming_data):
+    chunked_data = list(chunker(DATA, pixels_per_layer))
+
+    layers = []
+    for rawlayer in chunked_data:
+        layers.append(np.array(rawlayer).reshape((size_y, size_x)))
+    return layers
+
+
+def part_one(layers):
+    min_zeroes = 99999
+    least_zero_layer = None
+    for i, layer in enumerate(layers):
+        if (layer_zeroes := np.count_nonzero(layer == 0)) < min_zeroes:
+            min_zeroes = layer_zeroes
+            least_zero_layer = i
+
+    return np.count_nonzero(layers[least_zero_layer] == 1) * np.count_nonzero(layers[least_zero_layer] == 2)
+
+
+def part_two(layers):
+    final_image = np.zeros([size_y, size_x], dtype=np.int)
+
+    for y in range(size_y):
+        for x in range(size_x):
+            final_image[y, x] = get_pixel(y, x, layers)
+    return final_image
+
+
 size_x = 25
 size_y = 6
 
 pixels_per_layer = size_x * size_y
 
 DATA = [int(x) for x in read_data()]
-chunked_data = list(chunker(DATA, pixels_per_layer))
 
-layers = []
-for rawlayer in chunked_data:
-    layers.append(np.array(rawlayer).reshape((size_y, size_x)))
+layers = make_layers(DATA)
 
-
-min_zeroes = 99999
-least_zero_layer = None
-for i, layer in enumerate(layers):
-    if (layer_zeroes := np.count_nonzero(layer == 0)) < min_zeroes:
-        min_zeroes = layer_zeroes
-        least_zero_layer = i
-
-print(f"layer with the least zeroes is {least_zero_layer}")
-
-part_one_answer = np.count_nonzero(layers[least_zero_layer] == 1) * np.count_nonzero(layers[least_zero_layer] == 2)
-
-print(f"part one answer is {part_one_answer}")
-
-final_image = np.zeros([size_y,size_x], dtype=np.int)
-
-for y in range(size_y):
-    for x in range(size_x):
-        final_image[y, x] = get_pixel(y, x, layers)
+print(f"part one answer is {part_one(layers)}")
 
 with np.printoptions(linewidth=200, formatter={'int': lambda x: "X" if x == 1 else " "}):
-    print(final_image)
+    print(part_two(layers))
 
